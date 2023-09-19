1aab39a0441e4eca511e161b32b3f8d2d38bede3
Umar (OSU) <ujara4645@gmail.com>
2023-09-19 11:39:54 -0400

    deleted project to rename to it to something different from the app name

--- BrainRotDjango/BrainRotDjango/asgi.py
+++ BrainRotDjango/BrainRotDjango/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for VGRSystem project.
+ASGI config for BrainRotDjango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VGRSystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BrainRotDjango.settings')
 
 application = get_asgi_application()
--- BrainRotDjango/BrainRotDjango/wsgi.py
+++ BrainRotDjango/BrainRotDjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for VGRSystem project.
+WSGI config for BrainRotDjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VGRSystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BrainRotDjango.settings')
 
 application = get_wsgi_application()
