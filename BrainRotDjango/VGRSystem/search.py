517b359526997efb2a4b05fc8149202fa74970d7
Umar (OSU) <ujara4645@gmail.com>
2023-09-21 16:25:56 -0400

    more search script

--- BrainRotDjango/VGRSystem/search.py
+++ BrainRotDjango/VGRSystem/search.py
@@ -1,17 +1,18 @@
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 
-def search(genres):
+def search(title="", rating=0, developers=[], genres=[], summary="", platforms=[], 
+           plays=0, playing=0, backlogs=0, wishlist=0, lists=0, reviews=0, release_date=""):
     client = Elasticsearch("http://localhost:9200")
     s = Search(using=client)
-    s = s.filter('match', genres="First person shooter")    
+    s = s.filter('terms', genres=genres)    
     response = s.execute()
 
-    for hit in s:
+    for hit in s.scan():
         print(hit.genres)
 
     return response
 
-search("action")
+search(genres=["Adventure"])
 
 
