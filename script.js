62d9418e23a57ff7c534c5638fd5327b810bb512
Jimmy Wu <jimmy@Jimmys-MacBook-Air.local>
2023-09-26 16:26:05 -0400

    added slider and corresponding javaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+$( function() {
+  var handle = $( "#custom-handle" );
+  $( "#slider" ).slider({
+    create: function() {
+      handle.text( $( this ).slider( "value" ) );
+    },
+    slide: function( event, ui ) {
+      handle.text( ui.value );
+    }
+  });
+} );
