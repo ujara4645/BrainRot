3078f8abdb7d04477230430afb065975f5a777ec
Ervin Qalliaj <eeqalliaj@gmail.com>
2023-10-19 15:25:47 -0400

    Details Page

--- brain_rot/brain_rot_web/urls.py
+++ brain_rot/brain_rot_web/urls.py
@@ -6,5 +6,6 @@ app_name = "brain_rot_web"
 urlpatterns = [
     path("", views.index, name="index"),
     path("results", views.results, name="results"),
-    path("survey", views.survey, name="survey")
+    path("survey", views.survey, name="survey"),
+    path("results/<int:game_id>", views.result_detail, name="results_details")
 ]
--- brain_rot/brain_rot_web/views.py
+++ brain_rot/brain_rot_web/views.py
@@ -1,35 +1,46 @@
 import json
 from django.template import loader
 from django.http import HttpResponse
-from django.shortcuts import render 
+from django.shortcuts import render
 from utilities import search
+
 
 def index(request):
     # template = loader.get_template("brain_rot_web/index.html")
     context = {}
     return render(request, 'index.html', context)
 
+
 def survey(request):
     # template = loader.get_template("brain_rot_web/index.html")
     context = {}
     return render(request, 'survey.html', context)
 
+
 def results(request):
-    
     # template = loader.get_template("brain_rot_web/results.html")
     summary = request.GET["desc"]
     rating = ('gte', float(request.GET["rating"]))
     hits = search.search(summary=summary, rating=rating).hits
     results = {}
-    
+
     for hit in hits:
         results[hit.title] = {'Title': hit.title,
-                               'Rating': hit.rating, 
-                               'Plays': hit.plays, 
-                               'Release Date': hit.release_date,
-                               'Summary': hit.summary}
-        
+                              'Rating': hit.rating,
+                              'Plays': hit.plays,
+                              'Release Date': hit.release_date,
+                              'Summary': hit.summary,
+                              'Id': hit.meta.id}
+
     context = {'results': results}
-        
+
     return render(request, 'results.html', context)
-    
+
+
+def result_detail(request, game_id):
+    result = search.search_by_id(game_id)
+
+    context = {'result': result}
+
+
+    return render(request, 'result_details.html', context)
