532254e278b4c55244a9565d563120e4368ac691
Umar (OSU) <ujara4645@gmail.com>
2023-10-19 12:55:45 -0400

    random recommendations working

--- brain_rot/brain_rot_web/views.py
+++ brain_rot/brain_rot_web/views.py
@@ -20,16 +20,16 @@ def results(request):
     
     # template = loader.get_template("brain_rot_web/results.html")
 
-    if request.POST.get('button-type') == 'search':
+    if request.GET["query"] == 'form':
         summary = request.GET["desc"]
         rating = ('gte', float(request.GET["rating"]))
         hits = search.search(summary=summary, rating=rating).hits
     
-    elif request.POST.get('button-type') == 'random':
+    elif request.GET["query"] == 'random':
         rand_str = random.choices(string.ascii_lowercase, k=5)
         hits = []
         for c in rand_str:
-            hits.append(search.search(summary=c, rating=rating).hits[0])
+            hits.append(search.search(summary=c).hits[0])
 
     results = {}
     
